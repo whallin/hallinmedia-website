7c9dc8fded77b2edecf3e69b281fc03832b7e6c7
William Hallin <william@hallin.media>
2024-08-16 17:15:43 +0200

    feat: remove blog, add cv

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,5 @@
 import { z, defineCollection } from 'astro:content'
 
-const blogCollection = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		footnote: z.string(),
-		language: z.enum(['sv', 'en']),
-		image: z.object({
-			src: z.string(),
-			alt: z.string()
-		}),
-		publishDate: z.date()
-	})
-})
-
 const workCollection = defineCollection({
 	type: 'content',
 	schema: z.object({
@@ -28,6 +14,5 @@ const workCollection = defineCollection({
 })
 
 export const collections = {
-	blog: blogCollection,
 	work: workCollection
 }
