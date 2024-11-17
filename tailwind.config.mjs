4b34cdf0dbad4640dcd5f1979628d529b669620f
William Hallin <william@hallin.media>
2024-11-17 13:52:09 +0100

    refactor: :zap: use bun

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,15 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['DM Sans', ...defaultTheme.fontFamily.sans],
-				serif: ['DM Serif Text', ...defaultTheme.fontFamily.serif]
-			}
-		}
-	},
-	plugins: [require('@tailwindcss/typography')]
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["DM Sans", ...defaultTheme.fontFamily.sans],
+        serif: ["DM Serif Text", ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [],
+};
